dc33def7b3055b932ae0c1a8d6c9b90c8f86442e
SAAFIREEE <109060021+SAAFIREEE@users.noreply.github.com>
2026-06-15 00:51:11 +0630

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    // This helps Vercel find files more reliably
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
     },
   },
 });
